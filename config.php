d805a32dcd4ab53713810b8e40067a6dacb32024
Fatou Gueye NDONG <fatougueyendong@esp.sn>
2025-06-30 10:15:07 +0000

    Correction

--- config.php
+++ config.php
@@ -6,14 +6,16 @@ if (!$databaseUrl) {
 }
 
 $dbopts = parse_url($databaseUrl);
+
 $host = $dbopts["host"];
-$port = $dbopts["port"];
+$port = $dbopts["port"] ?? "5432"; // Valeur par défaut si non fournie
 $user = $dbopts["user"];
 $pass = $dbopts["pass"];
 $dbname = ltrim($dbopts["path"], "/");
 
 try {
-    $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $db = new PDO($dsn, $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die(json_encode(["error" => $e->getMessage()]));
