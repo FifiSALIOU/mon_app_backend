b005756420d6b34ab45ae3dcf595fbcbba3b9c05
Fatou Gueye NDONG <fatougueyendong@esp.sn>
2025-06-30 09:51:41 +0000

    Correction

--- config.php
+++ config.php
@@ -1,3 +1,20 @@
 <?php
-$db = new PDO('pgsql:host=localhost;dbname=private_space', 'postgres', 'password');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$databaseUrl = getenv("DATABASE_URL");
+
+if (!$databaseUrl) {
+    die(json_encode(["error" => "DATABASE_URL non définie"]));
+}
+
+$dbopts = parse_url($databaseUrl);
+$host = $dbopts["host"];
+$port = $dbopts["port"];
+$user = $dbopts["user"];
+$pass = $dbopts["pass"];
+$dbname = ltrim($dbopts["path"], "/");
+
+try {
+    $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die(json_encode(["error" => $e->getMessage()]));
+}
